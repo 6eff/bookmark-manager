c627f2b257a1ea4b041f487bc4ecd5eb680c1049
6eff <6eff@mail.ru>
2016-07-28 15:25:24 +0300

    adds tags model

---
+++ spec/features/adding_tags_spec.rb
@@ -0,0 +1,13 @@
+# require 'spec_helper'
+
+feature "adding tags" do
+  scenario 'adds a tags to a link' do
+    visit '/links/new'
+    fill_in "title", with: "google"
+    fill_in('url', with: 'www.google.com')
+    fill_in('tags', with: 'search')
+    click_button 'submit'
+    link = Link.first
+    expect(link.tags.map(&:name)).to include('search')
+  end
+end
