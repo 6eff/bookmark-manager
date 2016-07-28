835d48ed74d3ba925ed5ecc1f0e2d1513a6f3b63
6eff <6eff@mail.ru>
2016-07-28 22:25:51 +0300

    adds multiple tags to link

--- spec/features/adding_tags_spec.rb
+++ spec/features/adding_tags_spec.rb
@@ -1,6 +1,7 @@
 # require 'spec_helper'
 
 feature "adding tags" do
+
   scenario 'adds a tags to a link' do
     visit '/links/new'
     fill_in "title", with: "google"
@@ -9,5 +10,15 @@ feature "adding tags" do
     click_button 'submit'
     link = Link.first
     expect(link.tags.map(&:name)).to include('search')
+  end
+
+  scenario 'adds multiple tags to a link' do
+    visit '/links/new'
+    fill_in "title", with: "google"
+    fill_in('url', with: 'www.google.com')
+    fill_in('tags', with: 'search resources')
+    click_button 'submit'
+    link = Link.first
+    expect(link.tags.map(&:name)).to include('search', 'resources')
   end
 end
